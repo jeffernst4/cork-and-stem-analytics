12b3172430496f2863be884d6304b8a90aeaddfb
Jeff Ernst <jeffernst4@gmail.com>
2024-10-15 00:29:34 +0000

    update src_doordash_transactions with new table format

--- models/src/doordash/src_doordash_transactions.sql
+++ models/src/doordash/src_doordash_transactions.sql
@@ -1,32 +1,36 @@
 WITH
     raw_data AS (
+        SELECT *
+        FROM {{ source('doordash', 'doordash_transactions') }}
+    ),
+    final AS (
         SELECT
-            `Order ID` AS order_id,
-            `Store Name` AS store_name,
-            `Store ID` AS store_id,
-            `Business ID` AS business_id,
-            `Merchant Supplied ID` AS merchant_supplied_id,
-            `Street Address` AS street_address,
-            `City and State` AS city_and_state,
-            TIMESTAMP(CONCAT(SAFE.PARSE_DATE('%m/%d/%Y', `Order Placed Date`), ' ', `Order Placed Time`)) AS order_placed_timestamp,
-            TIMESTAMP(CONCAT(SAFE.PARSE_DATE('%m/%d/%Y', `Pickup Date`), ' ', `Pickup Time`)) AS pickup_timestamp,
-            TIMESTAMP(CONCAT(SAFE.PARSE_DATE('%m/%d/%Y', `Delivery Date`), ' ', `Delivery Time`)) AS delivery_timestamp,
-            `Was Cancelled` AS is_cancelled,
-            `Was Pickup` AS is_pickup,
-            `Was Dashpass` AS is_dashpass,
-            `Subtotal` AS subtotal,
-            `Order Protocol` AS order_protocol,
-            `POS Error Status` AS pos_error_status,
-            `POS Provider` AS pos_provider,
-            `Is Missing or Incorrect` AS is_missing_or_incorrect,
-            `Error Charge` AS error_charge,
-            `Commission` AS commission,
-            `Rating` AS rating,
-            `Total Item Count` AS total_item_count,
-            `Is Group Order` AS is_group_order,
-            `Currency` AS currency
+            order_id,
+            store_name,
+            store_id,
+            business_id,
+            merchant_supplied_id,
+            street_address,
+            city_and_state,
+            TIMESTAMP(CONCAT(SAFE.PARSE_DATE('%Y-%m-%d', order_placed_date), ' ', order_placed_time)) AS order_placed_timestamp,
+            TIMESTAMP(CONCAT(SAFE.PARSE_DATE('%Y-%m-%d', pickup_date), ' ', pickup_time)) AS pickup_timestamp,
+            TIMESTAMP(CONCAT(SAFE.PARSE_DATE('%Y-%m-%d', delivery_date), ' ', delivery_time)) AS delivery_timestamp,
+            is_cancelled,
+            is_pickup,
+            is_dashpass,
+            subtotal,
+            order_protocol,
+            pos_error_status,
+            pos_provider,
+            is_missing_or_incorrect,
+            error_charge,
+            commission,
+            rating,
+            total_item_count,
+            is_group_order,
+            currency
         FROM {{ source('doordash', 'doordash_transactions') }}
     )
 
 SELECT *
-FROM raw_data
+FROM final
