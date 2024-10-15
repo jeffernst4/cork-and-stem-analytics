341fe00dbe7309cb1a7eb34ea1eca491695909a3
Jeff Ernst <jeffernst4@gmail.com>
2024-10-15 20:17:21 +0000

    update evey_attendees src model

--- models/src/evey/src_evey_attendees.sql
+++ models/src/evey/src_evey_attendees.sql
@@ -1,7 +1,11 @@
 WITH
     raw_data AS (
+        SELECT *
+        FROM {{ source('evey', 'evey_attendees') }}
+    ),
+    final AS (
         SELECT
-            id AS attendee_id,
+            attendee_id,
             event_number AS event_id,
             event_title,
             order_id,
@@ -9,9 +13,11 @@ WITH
             email,
             phone,
             first_name,
-            last_name
-        FROM {{ source('evey', 'evey_attendees') }}
+            last_name,
+            {{ convert_to_pt("PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S %z', purchased_at)") }} AS purchase_timestamp,
+            {{ convert_to_pt('TIMESTAMP(eticket_sent_at)') }} AS ticket_sent_timestamp
+        FROM raw_data
     )
 
 SELECT *
-FROM raw_data
+FROM final
