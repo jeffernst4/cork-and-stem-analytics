efb395ce00930d8b1a5e2bd43c6289a4883272a9
Jeff Ernst <jeffernst4@gmail.com>
2026-02-03 04:55:54 +0000

    update doordash table

--- models/src/doordash/src_doordash_transactions.sql
+++ models/src/doordash/src_doordash_transactions.sql
@@ -7,17 +7,15 @@ WITH
 
     final AS (
         SELECT
+            business_id,
             doordash_order_id,
             doordash_transaction_id,
             transaction_type,
             store_name,
             store_id,
             business_id,
-            merchant_store_id,
             TIMESTAMP(SAFE.PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%E*S', timestamp_local_time)) AS order_timestamp,
-            final_order_status,
             subtotal,
-            currency
         FROM {{ source('doordash', 'doordash_transactions') }}
     )
 
